ed72cffd89446cceabe17665556d7d88eff2130e
Ravi <chauhan.ravi100891@gmail.com>
2023-04-14 00:38:04 +0530

    css to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header, Footer, Home, Shop, Men, Women, Blog, Contact, Error } from './components/layout/helper';
 
-import './css/index.css';
-import './css/main.css';
-
-// https://klbtheme.com/clotya/home-2/#
+import "./scss/main.scss";
 
 function App() {
 
